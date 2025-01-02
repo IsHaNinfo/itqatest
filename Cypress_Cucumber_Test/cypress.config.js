9c9409c8e93711a524c77c6efc7d84815b03a3f0
ishaninfo <ishaninfo56@gmail.com>
2025-01-02 13:02:51 +0530

    changes

--- Cypress_Cucumber_Test/cypress.config.js
+++ Cypress_Cucumber_Test/cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
+      allureWriter(on, config);
+      return config;
     },
+    
     baseUrlAPI: "http://localhost:7081",
   },
   chromeWebSecurity: false,
